90324ca0d7281b1e7818c562ecc84bd97e4b98a8
Vicente Marques <vicente.marques@tecnico.ulisboa.pt>
2024-08-05 20:24:36 +0100

    backend: basic page structure

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,23 @@ mongoose.connect("mongodb://localhost:27017/schrank")
 
 app.get('/', (req, res) => {
     res.send("Schrank!!");
-})
+});
+
+app.get('/closet', (req, res) => {
+    res.send("All the items should be here.");
+});
+
+app.get("/outfits", (req, res) => {
+    res.send("Page with outfits should be here");
+});
+
+app.get("/outfits/new", (req, res) => {
+    res.send("Form for new outfit");
+});
+
+app.get("/outfits/:id", (req, res) => {
+    res.send("Single outfit page");
+});
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
