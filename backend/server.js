7c2c3828de28ec9a4070e15759863a89f04c75b0
Vicente Marques <vicente.marques@tecnico.ulisboa.pt>
2024-08-05 20:14:44 +0100

    backend: connected to mongo

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb://localhost:27017/schrank")
+    .then(() => {
+        console.log("Connected to MongoDB!");
+    }).catch((error) => {
+        console.log("Error connecting to MongoDB: ", error);
+    });
 
 app.get('/', (req, res) => {
     res.send("Schrank!!");
