5519ab1c6c1c8075485c584d0d39f7ca907761c7
Vicente Marques <vicente.marques@tecnico.ulisboa.pt>
2025-06-13 20:29:08 +0100

    fix outfit sizing

--- frontend/src/components/outfits/OutfitList.tsx
+++ frontend/src/components/outfits/OutfitList.tsx
@@ -8,10 +8,10 @@ type Props = {
 
 export default function OutfitList({ outfits, refreshOutfits }: Props) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+    <>
       {outfits.map((outfit) => (
         <OutfitCard key={outfit._id} outfit={outfit} />
       ))}
-    </div>
+    </>
   );
 }
